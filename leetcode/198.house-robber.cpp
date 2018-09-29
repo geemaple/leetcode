b71d2d6b7f0eaaba9e7c24f9498c1ccc36bce67e
Dean Ji <geemaple@gmail.com>
2018-09-29 19:47:31 +0800

    198 another way of thinking

--- leetcode/198.house-robber.cpp
+++ leetcode/198.house-robber.cpp
@@ -23,3 +23,28 @@ public:
         return *max_element(table[m].begin(), table[m].end());
     }
 };
+
+// f(x) = max(f(x - 2) + steal, f(x - 1))
+class Solution2 {
+public:
+    int rob(vector<int>& nums) {
+        
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+
+        int m = nums.size();
+        vector<int> table(m + 1, 0);
+        table[1] = nums[0];
+
+        for(auto i = 2; i <= m; ++i)
+        {
+            table[i] = max(table[i - 2] + nums[i - 1], table[i - 1]);
+        }
+
+        return table[m];
+    }
+};
+
+
--- leetcode/198.house-robber.py
+++ leetcode/198.house-robber.py
@@ -21,3 +21,22 @@ class Solution(object):
             table[i][0] = max(table[i - 1][0], table[i - 1][1])
 
         return max(table[-1])
+
+# f(x) = max(f(x - 2) + steal, f(x - 1))
+class Solution2(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums) == 0:
+            return 0
+
+        m = len(nums)
+        table = [0 for _ in range(m + 1)]
+        table[1] = nums[0]
+
+        for i in range(2, m + 1):
+            table[i] = max(table[i - 2] + nums[i - 1], table[i - 1])
+
+        return table[-1]
