a555fcefd9e99be89c0a6decdc9e6a5a7fd53936
Xiangfeng Ji <geemaple@gmail.com>
2019-10-29 18:49:11 +0800

    322.coin-change

--- leetcode/322.coin-change.cpp
+++ leetcode/322.coin-change.cpp
@@ -1,23 +1,54 @@
-// f(x) = min(f(x - 1) + 1, f(x - 2) + 1, f(x - 5) + 1)
+/*
+ * @lc app=leetcode id=322 lang=cpp
+ *
+ * [322] Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (27.95%)
+ * Total Accepted:    261.6K
+ * Total Submissions: 810K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * You are given coins of different denominations and a total amount of money
+ * amount. Write a function to compute the fewest number of coins that you need
+ * to make up that amount. If that amount of money cannot be made up by any
+ * combination of the coins, return -1.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3 
+ * Explanation: 11 = 5 + 5 + 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * 
+ * 
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ */
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> table(amount + 1, INT_MAX);
         table[0] = 0;
-        for(auto i = 1; i <= amount; ++i)
-        {
-            int ans = INT_MAX;
-            for(auto j = 0; j < coins.size(); ++j)
-            {
+        
+        for (auto i = 1; i <= amount; ++i) {
+            for (auto j = 0; j < coins.size(); ++j) {
                 int pre = i - coins[j];
-                if (pre >= 0 && table[pre] != INT_MAX  && table[pre] + 1 < ans)
-                {
-                    ans = table[pre] + 1;
+                if (pre >= 0 && table[pre] != INT_MAX && table[pre] + 1 < table[i]) {
+                    table[i] = table[pre] + 1;
                 }
             }
-            table[i] = ans;
         }
         
-        return table[amount] == INT_MAX ? -1 : table[amount];
+        return table[amount] < INT_MAX ? table[amount] : -1;
     }
 };
--- leetcode/322.coin-change.py
+++ leetcode/322.coin-change.py
@@ -1,3 +1,39 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+# https://leetcode.com/problems/coin-change/description/
+#
+# algorithms
+# Medium (27.95%)
+# Total Accepted:    261.6K
+# Total Submissions: 810K
+# Testcase Example:  '[1,2,5]\n11'
+#
+# You are given coins of different denominations and a total amount of money
+# amount. Write a function to compute the fewest number of coins that you need
+# to make up that amount. If that amount of money cannot be made up by any
+# combination of the coins, return -1.
+# 
+# Example 1:
+# 
+# 
+# Input: coins = [1, 2, 5], amount = 11
+# Output: 3 
+# Explanation: 11 = 5 + 5 + 1
+# 
+# Example 2:
+# 
+# 
+# Input: coins = [2], amount = 3
+# Output: -1
+# 
+# 
+# Note:
+# You may assume that you have an infinite number of each kind of coin.
+# 
+#
 class Solution(object):
     def coinChange(self, coins, amount):
         """
@@ -5,12 +41,44 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        table = [float('inf') for i in range(amount + 1)]
+        table = [float('inf') for _ in range(amount + 1)]
         table[0] = 0
-            
-        for t in range(1, amount + 1):
-            for c in coins:
-                if t - c >= 0 and table[t - c] + 1 < table[t]:
-                    table[t] = table[t - c] + 1
-                    
-        return table[amount] if table[amount] != float('inf') else -1
+
+        for i in range(1, amount + 1):
+
+            for coin in coins:
+                if i - coin >= 0 and table[i - coin] + 1 < table[i]:
+                    table[i] = table[i - coin] + 1
+
+        return table[amount] if table[amount] < float('inf') else -1
+
+
+class Solution2(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        table = [float('inf') for _ in range(amount + 1)]
+        table[0] = 0
+        t = [0 for _ in range(amount + 1)]
+
+        for i in range(1, amount + 1):
+
+            for coin in coins:
+                if i - coin >= 0 and table[i - coin] + 1 < table[i]:
+                    table[i] = table[i - coin] + 1
+                    t[i] = coin
+
+        k = amount
+        print('combinations', end=' ')
+        while (t[k] > 0):
+            print(t[k], end=' ')
+            k = k - t[k]
+
+        return table[amount] if table[amount] < float('inf') else -1
+
+if __name__ == "__main__":
+    s = Solution2()
+    print(s.coinChange([2, 5, 7], 27))
