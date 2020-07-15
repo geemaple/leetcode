2226c8e934db7f364890f4b8f77d37bf26058488
Dean Ji <geemaple@gmail.com>
2020-07-15 08:42:23 +0800

    leetcode 9

---
+++ leetcode/9.palindrome-number.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (47.28%)
+ * Total Accepted:    943.6K
+ * Total Submissions: 2M
+ * Testcase Example:  '121'
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 121
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * Follow up:
+ * 
+ * Coud you solve it without converting the integer to a string?
+ * 
+ */
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        
+        int left = x;
+        int right = 0;
+        while (right < left) {
+            right = right * 10 + left % 10;
+            left = left / 10;
+        }
+            
+        return (left == right || left == right / 10);
+        
+    }
+};
--- leetcode/9.palindrome-number.py
+++ leetcode/9.palindrome-number.py
@@ -1,9 +1,50 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (47.28%)
+# Total Accepted:    943.6K
+# Total Submissions: 2M
+# Testcase Example:  '121'
+#
+# Determine whether an integer is a palindrome. An integer is a palindrome when
+# it reads the same backward as forward.
+# 
+# Example 1:
+# 
+# 
+# Input: 121
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+# 
+# 
+# Follow up:
+# 
+# Coud you solve it without converting the integer to a string?
+# 
+#
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
         
@@ -14,3 +55,4 @@ class Solution(object):
             x = x // 10
             
         return mirror == origin
+        
