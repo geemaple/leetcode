9cd4bbcfc2c22df67d3814ec84a60367e6384ee2
Dean Ji <geemaple@gmail.com>
2020-07-18 12:16:40 +0800

    leetcode 12

--- leetcode/12.integer-to-roman.py
+++ leetcode/12.integer-to-roman.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
         romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         res = ''
 
         for i in range(len(romans)):
-            while(num - values[i] >= 0):
+            while (num - values[i] >= 0):
                 res += romans[i]
                 num -= values[i]
 
