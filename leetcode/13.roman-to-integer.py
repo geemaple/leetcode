b7204b8502470482330246c932b50d0c91dfc348
Dean Ji <geemaple@gmail.com>
2020-07-20 08:08:41 +0800

    update 13

--- leetcode/13.roman-to-integer.py
+++ leetcode/13.roman-to-integer.py
@@ -1,23 +1,27 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        mapings = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
+class Solution:
+    def romanToInt(self, s: str) -> int:
 
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    res = 0
+    for i in range(0, len(s) - 1):
+        if roman[s[i]] < roman[s[i+1]]:
+            res -= roman[s[i]]
+        else:
+            res += roman[s[i]]
+
+    return res + roman[s[-1]]
+
+
+class Solution2:
+    def romanToInt(self, s: str) -> int:
+        
+        romans = ["M" , "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         res = 0
-        for i in range(len(s)):
-            res += mapings[s[i]]
-            if (i - 1 >= 0 and mapings[s[i]] > mapings[s[i - 1]]):
-                res -= mapings[s[i - 1]] * 2
+
+        for i in range(len(romans)):
+            while s.startswith(romans[i]):
+                res += values[i]
+                s = s[len(romans[i]): len(s)]
 
         return res
