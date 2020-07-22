b8160b30401c26017b1647298f39c7cb8fe241b8
Dean Ji <geemaple@gmail.com>
2020-07-22 08:29:01 +0800

    Update 15

--- leetcode/15.3sum.py
+++ leetcode/15.3sum.py
@@ -1,40 +1,34 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-
-        results = []
-        target = 0
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
         nums.sort()
 
-        for i in range(len(nums) - 1):
+        for i in range(len(nums) - 2):
+            
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
-            self.twoSum(nums, i + 1, nums[i], 0, results)
             
-        return results
+            a = nums[i]
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right: # two sum
 
-    def twoSum(self, nums, left, one, target, results):
-        
-        right = len(nums) - 1
-
-        while(left < right):
-            val = one + nums[left] + nums[right]
-            if val == target:
-                results.append([one, nums[left], nums[right]])
-                left += 1
-                right -= 1
-
-                while(left < right and nums[right] == nums[right + 1]):
+                if nums[left] + nums[right] == -a:
+                    res.append([a, nums[left], nums[right]])
+                    
+                    while (left < right and nums[left] == nums[left + 1]):
+                        left += 1
+                          
+                    while (left < right and nums[right] == nums[right - 1]):
                         right -= 1
 
-                while(left < right and nums[left] == nums[left - 1]):
-                        left += 1
-         
-            elif val < target:
-                left += 1
-            else:
-                right -= 1
+                    left += 1
+                    right -= 1
+                elif nums[left] + nums[right] > -a:
+                    right -= 1
+                else:
+                    left += 1
+                    
+        return res
