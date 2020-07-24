fee02f542524e9c355502ee1f93e3f5a37d981d4
Dean Ji <geemaple@gmail.com>
2020-07-24 09:32:44 +0800

    update leetcode 17

--- leetcode/17.letter-combinations-of-a-phone-number.py
+++ leetcode/17.letter-combinations-of-a-phone-number.py
@@ -1,38 +1,36 @@
-class Solution(object):
-    def __init__(self, *args, **kwargs):
-        self.mapping = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
-        }
+class Solution:
 
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+    digitToLetter = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
+
+    def letterCombinations(self, digits: str) -> List[str]:    
+        
         if digits is None or len(digits) == 0:
             return []
-
+        
         res = []
-        self.dfs(0, digits, "", res)
+        tmp = []
+        
+        self.helper(digits, 0, res, tmp)
         return res
+    
+    def helper(self, digits: str, start: int, res: List, tmp: List):
         
-
-    def dfs(self, level, digits, ans, res):
-        if level == len(digits):
-            res.append(ans)
+        if start == len(digits):
+            res.append(''.join(tmp))
             return
-
-        number = digits[level]
-        size = len(self.mapping[number])
-        for i in range(size):
-            ans += self.mapping[number][i]
-            self.dfs(level + 1, digits, ans, res)
-            ans = ans[:-1]
         
+        letters = self.digitToLetter[digits[start]]
+        for i in range(len(letters)):
+
+            tmp.append(letters[i])
+            self.helper(digits, start + 1, res, tmp)
+            tmp.pop()
