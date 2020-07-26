2ecffaa1f2543ea00acbfff0c83230cf3156376f
Dean Ji <geemaple@gmail.com>
2020-07-26 13:55:52 +0800

    update leetcode 18

--- leetcode/18.4sum.py
+++ leetcode/18.4sum.py
@@ -1,53 +1,91 @@
-# Your runtime beats 42.1 % of python submissions
-class Solution(object):
-    def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-
-        results = []
-        if len(nums) < 4:
-            return results
-        
-        nums.sort()
+from typing import List
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         res = []
-        
-        for i in range(len(nums)):
+        nums.sort()
+
+        for i in range(len(nums) - 3):
             
-            if i != 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            a = nums[i]
+            
+            for j in range(i + 1, len(nums) - 2): # 3sum
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                b = nums[j]
+                left = j + 1
+                right = len(nums) - 1
+                
+                while left < right: # two sum
+
+                    if a + b + nums[left] + nums[right] == target:
+                        res.append([a, b, nums[left], nums[right]])
+                        
+                        while (left < right and nums[left] == nums[left + 1]):
+                            left += 1
+                            
+                        while (left < right and nums[right] == nums[right - 1]):
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+                    elif a + b + nums[left] + nums[right] > target:
+                        right -= 1
+                    else:
+                        left += 1
+                    
+        return res
+
+class Solution2:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        k = 4
+        res = []
+        nums.sort()
+
+        if k > len(nums) or target < nums[0] * k or target > nums[-1] * k:
+            return []
+
+        self.kSum(nums, target, k, 0, [], res)
+        return res
+
+    def kSum(self, nums: List[int], target: int, k: int, start: int, ans: List[int], res: List[List]):
+    
+        if k == 2:
+            self.twoSum(nums, target, k, start, ans, res)
+            return
+
+        for i in range(start, len(nums) - k + 1):
+            if i > start and nums[i] == nums[i - 1]:
                 continue
             
-            for j in range(i + 1, len(nums)):
-                
-                if j != i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                
-                self.twoSum(nums, i, j, target, res)
-                
-        return res
-        
-    def twoSum(self,nums, i, j, target, results):
-        
-        start = j + 1
-        end = len(nums) - 1
-        
-        while(start < end):
-            tmp = nums[i] + nums[j] + nums[start] + nums[end]
-            if tmp == target:
-                results.append((nums[i], nums[j], nums[start], nums[end]))
-                
-                while start < end and nums[start] == nums[start + 1]:
-                    start += 1
-                    
-                while start < end and nums[end] == nums[end - 1]:
-                    end -= 1
-                    
-                start += 1
-                end -= 1
-                
-            elif tmp > target:
-                end -= 1
+            ans.append(nums[i])
+            self.kSum(nums, target, k - 1, i + 1, ans, res)
+            ans.pop()
+
+    def twoSum(self, nums: List[int], target: int, k: int, start: int, ans: List[int], res: List[List]):
+        left = start
+        right = len(nums) - 1
+
+        while left < right:
+            total = sum(ans) + nums[left] + nums[right]
+
+            if total == target:
+                ans.append(nums[left])
+                ans.append(nums[right])
+                res.append(ans.copy())
+                ans.pop()
+                ans.pop()
+
+                while (left < right and nums[left] == nums[left + 1]):
+                    left += 1
+                while (left < right and nums[right] == nums[right - 1]):
+                    right -= 1
+
+                left += 1
+                right -= 1
+            elif total > target:
+                right -= 1
             else:
-                start += 1
+                left += 1
