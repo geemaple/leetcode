ce95f21772036239983c3c5aaab5a60e0f4ef073
Dean Ji <geemaple@gmail.com>
2020-08-03 09:39:32 +0800

    update leetcode 25

--- leetcode/25.reverse-nodes-in-k-group.py
+++ leetcode/25.reverse-nodes-in-k-group.py
@@ -1,53 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def reverseKGroup(self, head, k):
-        """
-        :type head: ListNode
-        :type k: int
-        :rtype: ListNode
-        """
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         
-        if head is None:
-            return None
+        if k < 2:
+            return head
         
-        new_head = ListNode(0)
-        new_head.next = head
-
-        start = new_head
-        while start.next is not None:
-            end = start
-
-            for _ in range(k):
-                end = end.next
-                if end == None:
-                    return new_head.next
-
-            results = self.reverse(start, end)
-            start.next = results[0]
-            start = results[1]
-
-        return new_head.next
-
-    # start -> n1 -> n2 ... nk
-    # start -> nk -> nk-1 ... n1
-    # return nk -> nk-1 .... n1
-    def reverse(self, start, end):
+        cur = sentry = ListNode()
+        sentry.next = head
         
-        current = start.next
-        while start.next != end:
-            pre = current
-            current = current.next
-
-            pre.next = current.next
-            current.next = start.next
-            start.next = current
-
-            current = pre
-
-        return [start.next, current]
-
+        while cur is not None:
+            cur = self.reverse(cur, k)
+            
+        return sentry.next
+        
+        
+    def reverse(self, cur: ListNode, k: int) -> ListNode:
+        
+        head = tail = cur
+        res = head.next
+        
+        for i in range(k):
+            tail = tail.next
+            if tail is None:
+                return None
+         
+        for i in range(k - 1):
+            tmp = head.next
+            head.next = head.next.next
+            tmp.next = tail.next
+            tail.next = tmp
+            
+        return res
