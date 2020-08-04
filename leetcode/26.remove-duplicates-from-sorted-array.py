3cb53e65a07da4ae0a771b3bee56eacad75537d7
Dean Ji <geemaple@gmail.com>
2020-08-04 08:51:01 +0800

    update leetcode 26

--- leetcode/26.remove-duplicates-from-sorted-array.py
+++ leetcode/26.remove-duplicates-from-sorted-array.py
@@ -1,10 +1,5 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+    def removeDuplicates(self, nums: List[int]) -> int:        
         i = 0
         for j in range(len(nums)):
             if (i == 0 or nums[i - 1] < nums[j]):
@@ -12,3 +7,22 @@ class Solution(object):
                 i += 1
 
         return i
+
+
+class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        slow = fast = 0
+        
+        while fast < len(nums):
+                        
+            while fast + 1 < len(nums) and nums[fast] == nums[fast + 1]:
+                fast += 1
+            
+            if nums[slow] < nums[fast]:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            
+            slow += 1
+            fast += 1
+            
+        return slow
