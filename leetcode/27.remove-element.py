ae81b3451e46e1ed49a1feb5ddac0cfb5a5ce20f
Dean Ji <geemaple@gmail.com>
2020-08-05 09:02:48 +0800

    update leetcode 27

--- leetcode/27.remove-element.py
+++ leetcode/27.remove-element.py
@@ -1,17 +1,27 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-
-        k = 0
-        for i in range(len(nums)):
-
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-
-        return k
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
         
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+        
+        return slow
+        
+
+class Solution2:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            
+            if nums[start] == val:
+                nums[start], nums[end] = nums[end], nums[start]
+                end -= 1
+            else:
+                start += 1
+        
+        return start
