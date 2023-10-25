178744f232bc5949739ff7846f65116bf10870d6
Felix Ji <geemaple@gmail.com>
2023-10-25 18:33:30 +0800

    add color mark

--- leetcode/154.find-minimum-in-rotated-sorted-array-ii.cpp
+++ leetcode/154.find-minimum-in-rotated-sorted-array-ii.cpp
@@ -2,7 +2,7 @@
 //  Time: O(logN) ~ O(1)
 //  Space: O(1)
 //  Ref: -
-//  Note: Rotated ✅
+//  Note: Rotated 🟢
 
 //  Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
 //  
