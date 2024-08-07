d9179991a57570762230897e725d66277a452e20
felix <geemaple@gmail.com>
2024-08-07 17:06:18 +0800

    update 242

--- leetcode/242.valid-anagram.cpp
+++ leetcode/242.valid-anagram.cpp
@@ -1,88 +1,47 @@
-/*
- * @lc app=leetcode id=242 lang=cpp
- *
- * [242] Valid Anagram
- *
- * https://leetcode.com/problems/valid-anagram/description/
- *
- * algorithms
- * Easy (49.83%)
- * Total Accepted:    327.6K
- * Total Submissions: 632.5K
- * Testcase Example:  '"anagram"\n"nagaram"'
- *
- * Given two strings s and t , write a function to determine if t is an anagram
- * of s.
- * 
- * Example 1:
- * 
- * 
- * Input: s = "anagram", t = "nagaram"
- * Output: true
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: s = "rat", t = "car"
- * Output: false
- * 
- * 
- * Note:
- * You may assume the string contains only lowercase alphabets.
- * 
- * Follow up:
- * What if the inputs contain unicode characters? How would you adapt your
- * solution to such case?
- * 
- */
-// O(N) 12ms array access is much faster
+//  Tag: Hash Table, String, Sorting
+//  Time: O(N)
+//  Space: O(1)
+//  Ref: -
+//  Note: -
+
+//  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+//   
+//  Example 1:
+//  Input: s = "anagram", t = "nagaram"
+//  Output: true
+//  Example 2:
+//  Input: s = "rat", t = "car"
+//  Output: false
+//  
+//   
+//  Constraints:
+//  
+//  1 <= s.length, t.length <= 5 * 104
+//  s and t consist of lowercase English letters.
+//  
+//   
+//  Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+//  
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        int hash_count[26] = {0};
-        
         if (s.size() != t.size()) {
             return false;
         }
-        
-        for (int i = 0; i < s.size(); i++) {
-            hash_count[s[i] - 'a']++;
+        int n = s.size();
+        vector<int> count(26, 0);
+        for (int i = 0; i < n; i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        
-        for (int i = 0; i < t.size(); i++) {
-            hash_count[t[i] - 'a']--;
-        }
-        
+
         for (int i = 0; i < 26; i++) {
-            if (hash_count[i] != 0) {
+            if (count[i] != 0) {
                 return false;
             }
-            
         }
-        
         return true;
-    }
-};
-
-// O(N) 24ms
-class Solution2 {
-public:
-    bool isAnagram(string s, string t) {
-        
-        if (s.size() != t.size()) {
-            return false;
-        }
-        
-        unordered_map<char, int> count_s;
-        unordered_map<char, int> count_t;
-        
-        for (int i = 0; i < s.size(); i++) {
-            count_s[s[i]]++;
-            count_t[t[i]]++;
-        }
-        
-        return count_s == count_t;
     }
 };
--- leetcode/242.valid-anagram.py
+++ leetcode/242.valid-anagram.py
@@ -1,43 +1,45 @@
-#
-# @lc app=leetcode id=242 lang=python
-#
-# [242] Valid Anagram
-#
-# https://leetcode.com/problems/valid-anagram/description/
-#
-# algorithms
-# Easy (49.83%)
-# Total Accepted:    327.6K
-# Total Submissions: 632.5K
-# Testcase Example:  '"anagram"\n"nagaram"'
-#
-# Given two strings s and t , write a function to determine if t is an anagram
-# of s.
-# 
-# Example 1:
-# 
-# 
-# Input: s = "anagram", t = "nagaram"
-# Output: true
-# 
-# 
-# Example 2:
-# 
-# 
-# Input: s = "rat", t = "car"
-# Output: false
-# 
-# 
-# Note:
-# You may assume the string contains only lowercase alphabets.
-# 
-# Follow up:
-# What if the inputs contain unicode characters? How would you adapt your
-# solution to such case?
-# 
-#
-# O(N) 24ms
-from collections import Counter
+#  Tag: Hash Table, String, Sorting
+#  Time: O(N)
+#  Space: O(1)
+#  Ref: -
+#  Note: -
+
+#  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+#   
+#  Example 1:
+#  Input: s = "anagram", t = "nagaram"
+#  Output: true
+#  Example 2:
+#  Input: s = "rat", t = "car"
+#  Output: false
+#  
+#   
+#  Constraints:
+#  
+#  1 <= s.length, t.length <= 5 * 104
+#  s and t consist of lowercase English letters.
+#  
+#   
+#  Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+#  
+
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        n = len(s)
+        count = defaultdict(int)
+        for i in range(n):
+            count[s[i]] += 1
+            count[t[i]] -= 1
+
+        for k in count:
+            if count[k] != 0:
+                return False
+        return True
+
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -45,37 +47,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-
-        for l in set(s): # loop count <= 26
-            if s.count(l) != t.count(l):
-                return False
-
-        return True
-
-# O(NlogN) 60ms
-class Solution2(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
         return sorted(s) == sorted(t)
-
-# O(N) 48ms
-class Solution3(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # https://hg.python.org/cpython/file/6f535c725b27/Objects/dictobject.c#l1839
-        a = Counter(s)
-        b = Counter(t)
-
-        return a == b
-
-
