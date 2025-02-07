c6ad0ee84b0ed5ea8d682edac58300e85ed0e690
Felix <geemaple@gmail.com>
2025-02-07 23:56:11 +0800

    update video

--- leetcode/3160.find-the-number-of-distinct-colors-among-the-balls.cpp
+++ leetcode/3160.find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -3,6 +3,7 @@
 //  Space: O(N)
 //  Ref: -
 //  Note: -
+//  Video: https://youtu.be/-bM4tWPRfkc
 
 //  You are given an integer limit and a 2D array queries of size n x 2.
 //  There are limit + 1 balls with distinct labels in the range [0, limit]. Initially, all balls are uncolored. For every query in queries that is of the form [x, y], you mark ball x with the color y. After each query, you need to find the number of distinct colors among the balls.
